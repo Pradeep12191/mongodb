ccbf1c16a5e8e1dde5766d03e8bfa9e2c5726ec9
Pradeep Sivasubramanian <pxs6544@global.iff.com>
2017-10-19 13:20:13 +0530

    mongo client

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://pradeep:pradeep123@ds125555.mlab.com:25555/todoapp');
+mongoose.connect('mongodb://pradeep:pradeep123@ds125555.mlab.com:25555/todoapp', { useMongoClient: true });
 
 module.exports = { mongoose }
